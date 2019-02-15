b4868c64a9b13f2f3682a24ef8c9d697b4f98f80
Carl Hain <crhain@gmail.com>
2019-02-15 12:08:44 -0600

    Add Card component for use in AlbumDetail object

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
+import Card from './Card';
 
 const AlbumDetail = ({ album }) => {
   return (
-    <View>
+    <Card>
       <Text>{album.title}</Text>
-    </View>
+    </Card>
   );
 };
 
