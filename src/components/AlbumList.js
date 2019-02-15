01ae445269c4c21f4d0ed64c5f397a970eb01ce0
Carl Hain <crhain@gmail.com>
2019-02-15 11:32:04 -0600

    Render list of album titles from ajax call in AlbumList

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import Axios from 'axios';
 
 class AlbumList extends Component {
+  state = { albums: [] };
+
   componentWillMount() {
-    console.log('componentWillMount in AlbumList');
+    Axios.get('https://rallycoding.herokuapp.com/api/music_albums').then(
+      response => this.setState({ albums: response.data })
+    );
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album => {
+      return <Text key={album.title}>{album.title}</Text>;
+    });
   }
 
   render() {
-    return (
-      <View>
-        <Text>Album List!!!</Text>
-      </View>
-    );
+    console.log(this.state);
+    return <View>{this.renderAlbums()}</View>;
   }
 }
 
